ae0f8f0a934074065c668703cf41374c5845c30a
Park Soravee <park11102001@gmail.com>
2020-08-21 13:00:33 +0700

    add input

---
+++ Projects-ProgrammingFund1/1.cpp
@@ -0,0 +1,19 @@
+//1000 world chain
+#include<iostream>
+using namespace std;
+
+string str[1000];
+int strLength, N;
+
+void getStr();
+
+int main() {
+	cin >> strLength >> N;
+	getStr();
+	
+	return 0;
+}
+
+void getStr() {
+	
+}
